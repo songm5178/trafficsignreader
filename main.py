d004dd6f17cd25a19971bd36a11da5a4cc11857e
songm5178 <songm@rose-hulman.edu>
2016-02-04 18:47:31 -0500

    added main method :)

---
+++ main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+from PIL import Image
+import pytesseract
+import sys
+
+if __name__ == '__main__':
+    # read image
+    src = cv2.imread('traff1.jpg', cv2.IMREAD_COLOR)
+    
+    
+    # extract green color from HSV
+    
+    
+    
+    # extract traffic sign shape
+    
+    
+    # run tesseract on traffic sign
+
+    
