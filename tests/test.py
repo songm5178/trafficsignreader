269a5f6550e2125c2f4331b99a179b276b8dfb6b
songm5178 <songm@rose-hulman.edu>
2016-02-04 18:24:38 -0500

    re-organized files

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,13 @@
 import numpy as np
 import cv2
+from PIL import Image
 import pytesseract
 
 # Load an color image in grayscale
-img = cv2.imread('hsvcolor.png',cv2.IMREAD_COLOR)
+img = cv2.imread('alphabets.jpg',0)
 
+message = pytesseract.image_to_string(Image.open('alphabets.jpg'))
+print message
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
