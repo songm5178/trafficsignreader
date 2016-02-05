ed72a718acdff2e8c394c0f3b856c880a066a397
songm5178 <songm@rose-hulman.edu>
2016-02-04 19:56:38 -0500

    testing extracting hsv color space

--- main.py
+++ main.py
@@ -3,15 +3,47 @@ import cv2
 from PIL import Image
 import pytesseract
 import sys
+from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     # read image
     src = cv2.imread('traff1.jpg', cv2.IMREAD_COLOR)
-    
-    
+    dst = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)  
+#    cv2.imshow('image', dst)
+#    cv2.waitKey(0)
+#    cv2.destroyAllWindows()
+   plt.imshow(dst, cmap = 'gray', interpolation = 'bicubic')
+   plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+   plt.show()    
+
     # extract green color from HSV
+
     
+    # ij_index = [-1, -1] 
+    # max_h = -1
+    # for i in range (dst.shape[0]):
+        # for j in range (dst.shape[1]):
+
+            # if (dst[i,j][0] >= 35 and dst[i,j][0] < 40 and \
+                # dst[i,j][1] >= 40 and dst[i,j][2] < 50 and dst[i,j][2] > 240):
+                # cv2.circle(dst,(j, i),5,(255, 0, 255))
+
+            # else:
+                # cv2.circle(dst,(j, i),5,(0,0,0))
+    print dst[300, 190]
+    print dst[301, 190]
+    lower_green = np.array([50, 38, 100])
+    upper_green = np.array([150, 255, 255])
+    green_mask = cv2.inRange(dst, lower_green, upper_green)
+    res = cv2.bitwise_and(src, src, mask= green_mask)
+  
     
+    cv2.imshow('src',src)
+    cv2.imshow('mask', green_mask)
+    cv2.imshow('res', res)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
     
     # extract traffic sign shape
     
