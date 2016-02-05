06946a2475e9ff373d27469255bd3e18d27380f0
songm5178 <songm@rose-hulman.edu>
2016-02-04 21:31:36 -0500

    added blur

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     #### Initial Tesseract
-    message = pytesseract.image_to_string(Image.open('traff2.jpg'))
+    filename = 'traff2.jpg'
+    message = pytesseract.image_to_string(Image.open(filename))
     print "=====Colored Image, tesseract output======"
     print message
     
     #### read image
-    src = cv2.imread('traff2.jpg', cv2.IMREAD_COLOR)
-    dst = cv2.cvtColor(src, cv2.COLOR_RGB2HSV)  
+    src = cv2.imread(filename, cv2.IMREAD_COLOR)
+    blur = np.ones((src.shape[0],src.shape[1]), np.uint8)
+    cv2.medianBlur(src, 3, src)
+    cv2.imshow('blurred', src)
+    
+    dst = cv2.cvtColor(src, cv2.COLOR_RGB2HSV) 
+    
+
+    
     ## Use this to find coordinates #####
     plt.imshow(dst, cmap = 'gray', interpolation = 'bicubic')
     plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
@@ -23,11 +31,16 @@ if __name__ == '__main__':
 
     
     # This is how you look for HSV values
-    print dst[350, 550]
-    print dst[95, 576]
-    print dst[194, 248]
-    # print dst[200, 100]
-    lower_green = np.array([30, 220, 75])
+    print dst[200, 400]
+    print dst[350, 580]
+    print dst[358, 284]
+    
+    print dst[245, 600]
+    print dst[254, 260]
+    print dst[260, 397]
+    print dst[362, 422]
+
+    lower_green = np.array([30, 170, 67])
     upper_green = np.array([50, 255, 160])
     green_mask = cv2.inRange(dst, lower_green, upper_green)
     res = cv2.bitwise_and(src, src, mask= green_mask)
@@ -39,9 +52,15 @@ if __name__ == '__main__':
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
+    
+    
+    
     #### Morphology
     
     ## FUTURE WORK TODO: scale the kernel by size of traffic sign
+    cv2.findContours
+    
+    
     cv2.imwrite('mask.jpg', green_mask)
 
     message = pytesseract.image_to_string(Image.open('mask.jpg'))
