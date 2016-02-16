85515987f486c83d33b03ca004a60162d0c4fea4
cjysqpb <cjysqpb@hotmail.com>
2016-02-16 00:27:22 -0500

    MaskV2 Updated

--- tests/maskV2.py
+++ tests/maskV2.py
@@ -25,7 +25,7 @@ def crop2uint8(inputmat):
 if __name__ == '__main__':
     
     #### Initial Tesseract
-    filename = 'IMG_3467.jpg'
+    filename = 'traff2.jpg'
 
     
     #### read image
@@ -53,14 +53,16 @@ if __name__ == '__main__':
     
     result=255-result
     result=np.double(result)
+    
     r=np.size(result,0)
     c=np.size(result,1)
-    result_localAvg = cv2.blur(result,(r/3,c/3))
+    result_localAvg = cv2.blur(result,(r/3,c/3),None,None,cv2.BORDER_REPLICATE)
     shape=(r,c)
     mask=np.ones(shape)
     T=0.5
     mask[np.less_equal(result,result_localAvg*(1-T))]=0
     mask=1-mask
+    mask=255*mask
     
     kernel = np.ones((4,4),np.uint8)
     mask_morph = cv2.dilate(mask, kernel, iterations = 1)
@@ -71,8 +73,12 @@ if __name__ == '__main__':
     
     cv2.imwrite('mask.png', mask)
     cv2.imwrite('morphed_mask.png', mask_morph)
-    
+
     print "=====Mask tesseract output======"
+    message = pytesseract.image_to_string(Image.open('mask.png'))
+    print(message)
+    
+    print "=====Mask tesseract output After morphed======"
     message = pytesseract.image_to_string(Image.open('morphed_mask.png'))
     print(message)
     
